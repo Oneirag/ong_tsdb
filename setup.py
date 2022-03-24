72df63f370e89bd65b5d380e1c2263ca370dc016
Oscar Neira Garcia <oneirag@yahoo.es>
2022-03-24 22:30:35 +0100

    promote to version 0.6.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='ong_tsdb',
-    version='0.6.3',
+    version='0.6.4',
     packages=['ong_tsdb'],
     url='www.neirapinuela.es',
     license='',
