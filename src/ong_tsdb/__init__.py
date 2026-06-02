56cafc6bb10ee2fadfa10aad19d8a591d36faa74
Oscar Neira <oscar.neira@bau360.es>
2026-06-02 06:30:49 +0200

    chore: bump version to 0.8.0

--- src/ong_tsdb/__init__.py
+++ src/ong_tsdb/__init__.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 from ong_utils import OngConfig, LOCAL_TZ
 
-__version__ = "0.7.3"  # Version of the software
+__version__ = "0.8.0"  # Version of the software
 
 __cfg = OngConfig("ong_tsdb")
 config = __cfg.config
@@ -14,7 +14,11 @@ BASE_DIR = os.path.expanduser(config("BASE_DIR", "~/ong_tsdb"))
 COMPRESSION_EXT = ".gz"
 # DTYPE = np.float32  # Dates would end if 2038 if used float32
 DTYPE = np.float64
-DTYPE = np.float32  # Now trying storing not the full TS but the offset with the TS start...
-CHUNK_ROWS = 2 ** 14
-HTTP_COMPRESS_THRESHOLD = 1024  # Minimum number of data to activate compression in client and server
+DTYPE = (
+    np.float32
+)  # Now trying storing not the full TS but the offset with the TS start...
+CHUNK_ROWS = 2**14
+HTTP_COMPRESS_THRESHOLD = (
+    1024  # Minimum number of data to activate compression in client and server
+)
 HELLO_MSG = f"Hello from Ong_Tsdb server\nVersion {__version__}"
